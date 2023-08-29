eb1c0079d79b4bd45a41770025f6b2733f246f71
jackyonderbrack <mike.projektowanie@gmail.com>
2023-08-29 10:59:21 +0200

    package.json update and adding props

--- src/lit-app.ts
+++ src/lit-app.ts
@@ -3,15 +3,25 @@ import {customElement, property} from 'lit/decorators.js';
 
 @customElement('lit-app')
 export class LitApp extends LitElement {
+    @property() API_URL: string = 'https://jsonplaceholder.typicode.com/todos';
+    @property() data: Array<any> = []
+    @property() showForm: boolean = false
 
-    @property() name = 'Lit App';
+    constructor() {
+        super() 
+        this.API_URL
+        this.data
+        this.showForm
+        
+    }
 
     render() {
-        return html`<h1>Hello, ${this.name}</h1>`;
+        return html`
+        <h1>Wyświetlanie listy z API:</h1>
+        <p>${this.data}</p> 
+        `;
     }
 
     static styles = css`
-    h1{
-        color: red;
-    }`
+    `
 }
